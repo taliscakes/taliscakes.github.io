68c3ce5de13eff515718ef83d57c1efb3d7dd025
taliscakes <64445637+taliscakes@users.noreply.github.com>
2020-07-17 12:19:32 -0700

    Add files via upload

---
+++ titleScript.js
@@ -0,0 +1,14 @@
+
+// Build and show the main menu items
+function showHeader(logoPath)
+{ 
+  document.writeln ('<img src="'+logoPath+'images/cupcakeWithCherrywithLine.gif" width="75" height="70"><img src="'+logoPath+'images/taliscakes.gif" width="300" height="55"><br>
+        <img src="'+logoPath+'images/handCraftedUp.gif" width="400" height="55">');
+ document.writeln ('fdgfdgfdgf'); 
+  
+	 //document.writeln ('<img src="'+logoPath+'images/pageHeading1.gif">');
+	 	 
+} 
+
+//-->
+<!-- </script>
